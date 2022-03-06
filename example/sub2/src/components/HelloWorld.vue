b3d2c8a1e20f7c764e29e5254f22ca4d961db486
Labbott <1006762873@qq.com>
2022-03-06 17:44:24 +0800

    init

---
+++ example/sub2/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+     <div style="color: green"> 微前端子应用2</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld2',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ example/sub2/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+let app;
+
+let render = () => {
+  app = new Vue({
+    render: (h) => h(App),
+  })
+  app.$mount('#app')
+}
+render();
+
